63af19bb9b7cbcf57ed32c42c0f9050637af3486
sparkyflashrebob <125766704+sparkyflashrebob@users.noreply.github.com>
2026-08-21 09:28:40 -0700

    security: removed .obsidian/plugins/ from git tracking to protect credentials

--- .obsidian/plugins/GoogleBardObsidian-master/GoogleBardObsidian-master/src/components/memoizedMarkdownReact.tsx
+++
@@ -1,5 +0,0 @@
-import { FC, memo } from "react";
-import ReactMarkdown, { Options } from "react-markdown";
-
-const MemoizedReactMarkdown: FC<Options> = memo(ReactMarkdown);
-export default MemoizedReactMarkdown;
--- .obsidian/plugins/GoogleBardObsidian-master/GoogleBardObsidian-master/src/settings.ts
+++
@@ -1,7 +0,0 @@
-export interface BardObsidianSettings {
-	Cookie: string;
-}
-
-export const DEFAULT_SETTINGS: BardObsidianSettings = {
-	Cookie: "default",
-};
